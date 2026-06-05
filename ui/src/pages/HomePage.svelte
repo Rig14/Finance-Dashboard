4e2a918e3f851dabc380e3e70358ee1758aa9a69
Rihard Rivis <rihard.rivis@proton.me>
2026-06-05 16:27:33 +0300

    user can start the authentication flow to the LHV bank

--- ui/src/pages/HomePage.svelte
+++ ui/src/pages/HomePage.svelte
@@ -1,7 +1,16 @@
 <script lang="ts">
   import {t} from 'i18n'
+  import api from 'src/api/api'
+  import type {AuthorizationResponse} from 'src/api/types'
+
+  async function onclick() {
+    const res = await api.get<AuthorizationResponse>("enable-banking/auth")
+    window.location.href = res.url
+  }
 </script>
 
 <main class="text-center pt-10">
-  <h1>{t.title}</h1>
+  <button {onclick}>
+    {t.general.authenticate}
+  </button>
 </main>
